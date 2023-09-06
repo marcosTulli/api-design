005db5b58e2e88fd7721aa82c1510b9365ddec39
Marcos Tulli <marcos.tulli@raona.com>
2023-09-06 13:20:14 -0300

    Remove comments

--- server/server.js
+++ server/server.js
@@ -1,13 +1,3 @@
-// TODO: user app.params to find the lion using the id
-// and then attach the lion to the req object and call next. Then in
-// '/lion/:id' just send back req.lion
-
-// create a middleware function to catch and handle errors, register it
-// as the last middleware on app
-
-// create a route middleware for POST /lions that will increment and
-// add an id to the incoming new lion object on req.body
-
 const express = require('express');
 const app = express();
 const _ = require('lodash');
